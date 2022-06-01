62661b5743a56c8a3276ab78f958e8c46b96e71c
NinV <quannl71290@gmail.com>
2022-06-01 15:43:50 +0900

    cbf training

--- cil_tools/test_cil.py
+++ cil_tools/test_cil.py
@@ -10,19 +10,27 @@ import libs
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a recognizer')
     parser.add_argument('config', help='train config file path')
-    parser.add_argument('--work-dir', help='the dir to save logs and models')
-    # parser.add_argument('--resume-from', help='the checkpoint file to resume from')
-    args = parser.parse_args()
-    return args
 
+    # other configs
+    parser.add_argument('--testing_videos_per_gpu', type=int)
+    parser.add_argument('--testing_workers_per_gpu', type=int)
+    args = parser.parse_args()
+
+    # cfg_dict are used for updating the configurations from config file
+    cfg_dict = {}
+    for k, v in vars(args).items():
+        if v is not None and k != 'config':
+            cfg_dict[k] = v
+    return args, cfg_dict
 
 def main():
     import logging
     logging.getLogger("pytorch_lightning").setLevel(logging.WARNING)    # disable some redundant warning
 
-    args = parse_args()
+    args, cfg_dict = parse_args()
     config = Config.fromfile(args.config)
-    trainer = CILTrainer(config)
+    config.merge_from_dict(cfg_dict)
+    trainer = CILTrainer(config, dump_config=False)
     trainer.cil_testing()
 
 
