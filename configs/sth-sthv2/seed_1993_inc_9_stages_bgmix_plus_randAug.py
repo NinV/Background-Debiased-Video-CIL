6bed5faade1630b653e60b823a29d614703ee245
NinV <quannl71290@gmail.com>
2022-08-09 11:30:01 +0900

    sth-sth configs

---
+++ configs/sth-sthv2/seed_1993_inc_9_stages_bgmix_plus_randAug.py
@@ -0,0 +1,225 @@
+# base settings
+gpu_ids = 4
+
+# single gpu setting for traning
+videos_per_gpu = 12
+workers_per_gpu = 2
+accumulate_grad_batches = 1
+
+# single gpu setting for testing
+testing_videos_per_gpu = 1
+testing_workers_per_gpu = 2
+
+work_dir = 'work_dirs/sth-sthv2-seed_1993'
+
+task_splits = [
+    [156, 9, 119, 163, 54, 168, 157, 155, 27, 6, 51, 59, 42, 173, 113, 35, 118, 147, 159, 4, 2, 10, 112, 101, 134, 26,
+     57, 92, 154, 87, 106, 88, 17, 126, 85, 23, 72, 43, 68, 171, 32, 151, 91, 78, 19, 94, 36, 11, 145, 148, 8, 103, 3,
+     28, 135, 125, 167, 169, 97, 56, 49, 164, 44, 110, 62, 137, 166, 165, 55, 80, 12, 107, 66, 158, 15, 98, 122, 79,
+     104, 109, 52, 153, 40, 100],
+    [65, 152, 64, 129, 45, 77, 89, 150, 90, 71],
+    [170, 74, 30, 99, 96, 84, 67, 50, 138, 69],
+    [21, 18, 58, 108, 116, 136, 1, 144, 143, 83],
+    [81, 141, 60, 13, 0, 14, 142, 63, 117, 70],
+    [111, 5, 22, 16, 131, 121, 38, 41, 82, 127],
+    [114, 20, 31, 53, 37, 172, 130, 149, 86, 76],
+    [24, 34, 162, 33, 128, 146, 139, 120, 140, 102],
+    [47, 25, 123, 46, 61, 7, 115, 75, 133, 160],
+    [105, 132, 124, 48, 73, 93, 39, 95, 29, 161]]
+
+# select one of ['base', 'oracle', 'finetune']
+methods = 'base'
+starting_task = 0
+ending_task = 9
+use_nme_classifier = False
+use_cbf = False
+cbf_train_backbone = False
+budget_size = 20
+storing_methods = 'videos'
+budget_type = 'class'
+num_epochs_per_task = 50
+
+"""
+mode: bgmix_plus_randAug 
+    bgmix_prob = 1 - randAug_prob
+
+mode: randAug only
+    randAug_prob = 2    # set any value >= 1.0
+
+mode: bgmix only
+    randAug_prob = -1   # set any value < 0.0
+"""
+randAug_prob = 0.75     # bgmix_prob = 0.25
+
+# mmaction2 model config
+# model settings
+starting_num_classes = len(task_splits[0])
+model = dict(
+    type='CILRecognizer2D',
+    backbone=dict(
+        type='ResNetTSM',
+        pretrained='https://download.pytorch.org/models/resnet50-0676ba61.pth',
+        depth=50,
+        norm_eval=False,
+        num_segments=8,
+        shift_div=8),
+    cls_head=dict(
+        type='IncrementalTSMHead',
+        num_classes=starting_num_classes,
+        in_channels=2048,
+        inc_head_config=dict(type='LocalSimilarityClassifier',
+                             out_features=starting_num_classes,
+                             nb_proxies=1),
+        num_segments=8,
+        loss_cls=dict(type='LSCLoss'),
+        spatial_type='avg',
+        consensus=dict(type='AvgConsensus', dim=1),
+        dropout_ratio=0.5,
+        init_std=0.001,
+        is_shift=True,
+    ),
+    # model training and testing settings
+    train_cfg=None,
+    test_cfg=dict(average_clips='prob'))
+
+kd_modules_names = ['backbone.layer1', 'backbone.layer2', 'backbone.layer3', 'backbone.layer4', 'cls_head.avg_pool']
+repr_hook = 'cls_head.avg_pool'  # extract representation
+kd_exemplar_only = False
+# cil optimizer and lr_scheduler
+optimizer = dict(
+    type='SGD',
+    constructor='CILTSMOptimizerConstructorImprovised',
+    paramwise_cfg=dict(fc_lr_scale_factor=5.0),
+    lr=0.01,
+    momentum=0.9,
+    weight_decay=0.0001)
+optimizer_config = dict(grad_clip=dict(max_norm=20, norm_type=2))
+lr_scheduler = dict(type='MultiStepLR', params=dict(milestones=[20, 30], gamma=0.1))
+
+# cbf optimizer and lr_scheduler
+cbf_num_epochs_per_task = 50
+cbf_optimizer = dict(
+    type='SGD',
+    constructor='CILTSMOptimizerConstructorImprovised',
+    paramwise_cfg=dict(fc_lr_scale_factor=5.0),
+    lr=0.01,
+    momentum=0.9,
+    weight_decay=0.0001)
+cbf_lr_scheduler = dict(type='MultiStepLR', params=dict(milestones=[20, 30], gamma=0.1))
+
+# dataset settings
+data_root = '/local_datasets/something-somethingv2/rawframes/'
+train_ann_file = '/local_datasets/something-somethingv2/sthv2_train_list_rawframes.txt'
+val_ann_file = '/local_datasets/something-somethingv2/sthv2_val_list_rawframes.txt'
+cil_ann_file_template = '{}_task_{}.txt'  # requre exact 2 placeholders
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
+
+train_pipeline = [
+    dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8),
+    dict(type='RawFrameDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='RandAugment', n=2, m=10, prob=randAug_prob),
+    dict(
+        type='MultiScaleCrop',
+        input_size=224,
+        scales=(1, 0.875, 0.75, 0.66),
+        random_crop=False,
+        max_wh_scale_gap=1,
+        num_fixed_crops=13),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCHW'),
+    dict(type='Collect', keys=['imgs', 'label', 'randAug'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs', 'label'])
+]
+val_pipeline = [
+    dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8, test_mode=True),
+    dict(type='RawFrameDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCHW'),
+    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs'])
+]
+
+test_pipeline = [
+    dict(
+        type='SampleFrames',
+        clip_len=1,
+        frame_interval=1,
+        num_clips=8,
+        test_mode=True),
+    dict(type='RawFrameDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    # dict(type='CenterCrop', crop_size=224),
+    dict(type='ThreeCrop', crop_size=256),
+    # dict(type='TenCrop', crop_size=256),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCHW'),
+    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs'])
+]
+
+# for extracting features to construct exemplar set
+# this pipeline should be similar to validation pipeline if only run one epoch
+# In case we ran multiple epochs, this pipeline should be similar to train pipeline
+features_extraction_pipeline = [
+    dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8, test_mode=True),
+    dict(type='RawFrameDecode'),
+    dict(type='Resize', scale=(-1, 256)),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='Resize', scale=(224, 224), keep_ratio=False),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FormatShape', input_format='NCHW'),
+    # dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+    dict(type='ToTensor', keys=['imgs', 'label'])
+]
+
+dataset_type = 'BackgroundMixDataset'
+background_dir = '/local_datasets/something-somethingv2/bg_extract'
+data = dict(
+    train=dict(
+        type=dataset_type,
+        ann_file='',  # need to update this value before using
+        bg_dir=background_dir,
+        data_prefix=data_root,
+        pipeline=train_pipeline,
+        alpha=0.5
+    ),
+    val=dict(
+        type=dataset_type,
+        ann_file='',  # need to update this value before using
+        bg_dir=background_dir,
+        data_prefix=data_root,
+        pipeline=val_pipeline,
+        test_mode=True),
+    test=dict(
+        type=dataset_type,
+        ann_file='',  # need to update this value before using
+        bg_dir=background_dir,
+        data_prefix=data_root,
+        pipeline=test_pipeline,
+        test_mode=True),
+
+    features_extraction=dict(
+        type=dataset_type,
+        ann_file='',  # need to update this value before using
+        bg_dir=background_dir,
+        data_prefix=data_root,
+        pipeline=features_extraction_pipeline,
+        test_mode=True),
+    features_extraction_epochs=1,  # this value should be set to 1 if there's no randomness in pipeline
+
+    exemplar=dict(
+        type=dataset_type,
+        ann_file='',  # need to update this value before using
+        bg_dir=background_dir,
+        data_prefix=data_root,
+        pipeline=train_pipeline),
+)
+
+keep_all_backgrounds = False
+cbf_full_bg = False
