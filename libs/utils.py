e03bb862a148e22d52ba0a752b140349f3c9027a
NinV <quannl71290@gmail.com>
2022-09-08 14:32:12 +0900

    fix bug

--- libs/utils.py
+++ libs/utils.py
@@ -40,7 +40,7 @@ def print_mean_accuracy(accuracies: List[AverageMeter], num_classes_per_task, fl
     avg_acc = []
     for task_i in range(num_tasks):
         num_placeholders = num_tasks - task_i - 1
-        table.append(['task {}'.format(task_i), *accuracies[task_i].values] + num_placeholders * [None] + [avg_acc_task_i])
+        table.append(['task {}'.format(task_i), *accuracies[task_i].values] + num_placeholders * [None] + [accuracies[task_i].avg])
         avg_acc.append(accuracies[task_i].avg)
 
     avg_acc = np.mean(avg_acc)
@@ -57,19 +57,3 @@ def build_lr_scheduler(optimizer, config: mmcvConfig):
     constructor = scheduler_types[config.type]
     lr_scheduler = constructor(optimizer, **config['params'])
     return lr_scheduler
-
-
-if __name__ == '__main__':
-    print(print_mean_accuracy(
-        accuracies=[
-            [80.88003993034363],
-            [72.55107164382935, 89.23512697219849],
-            [68.30801367759705, 68.83852481842041, 90.04974961280823],
-            [65.4269278049469, 61.18980050086975, 62.68656849861145, 91.73789024353027],
-            [68.88422966003418, 62.0396614074707, 54.47761416435242, 71.22507095336914, 91.05263352394104],
-            [58.19801092147827, 50.70821642875671, 53.98010015487671, 50.71225166320801, 65.78947305679321,
-             88.65979313850403]
-        ],
-        num_classes_per_task=[51, 10, 10, 10, 10, 10],
-        floatfmt=".2f"
-    ))
